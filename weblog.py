4900c20cceeb394a2269cb28b3fdc2559e717a8b
Eduard Nicodei <eddnicodei@gmail.com>
2022-08-09 23:02:35 +0100

    Working with mistune.

--- weblog.py
+++ weblog.py
@@ -1,52 +1,61 @@
-import markdown
 import pathlib
-import pdb
 import argparse
-import collections
+import mistune
 
-class LinkAgreggator(markdown.treeprocessors.Treeprocessor):
-    def run(self, root):
+class LinkAgreggator(mistune.InlineParser):
+    def __init__(self, renderer, hard_wrap=True):
+        super().__init__(renderer, hard_wrap)
         self.links = []
-        self.agreggate_links(root)
 
-    def agreggate_links(self, element):
-        for child in element:
-            if child.tag == 'a':
-                self.links.append(child.attrib['href'])
-            self.agreggate_links(child)
+    def parse_link(self, m, state):
+        super().parse_link(m, state)
+
+        tokens = state.tokens
+        # TODO: understand / document how inline parsing works and
+        # how it produces tokens.
+        for token in tokens:
+            if token['type'] == 'image':
+                # Ignore image links
+                return
+            if token['type'] == 'link':
+                url = token['attrs']['url']
+                if url not in self.links:
+                    self.links.append(url)
+                return
 
 class Page:
     def __init__(self):
         self.body = None
         self.links = None
         self.backlinks = []
+        self.path = None
 
 
 def parse_weblog(top_path):
     weblog = {}
-    # Initial conversion
+
     for page_path in pathlib.Path(top_path).glob('**/*.md'):
         page = '/' + str(page_path.relative_to(top_path)).replace('.md', '.html')
         print(f'Processing {page}')
         weblog[page] = Page()
-        parser = markdown.Markdown()
-        link_agreggator = LinkAgreggator(parser)
-        parser.treeprocessors.register(link_agreggator, 'links', 1)
-        with open(page_path, "r", encoding="utf-8") as input_file:
-            weblog[page].body = parser.convert(input_file.read())
+        weblog[page].path = page_path
+
+        # Create a fresh parser for each page.
+        link_agreggator = LinkAgreggator(mistune.HTMLRenderer(escape=True))
+        md = mistune.Markdown(renderer=link_agreggator.renderer,
+                              inline=link_agreggator)
+
+        with open(page_path, 'r', encoding='utf-8') as input_file:
+            weblog[page].body = md.parse(input_file.read())
         weblog[page].links = link_agreggator.links
 
     for p, v in weblog.items():
-        print(p)
-        print(v.links)
         for link in v.links:
             if not link.startswith('http'):
-                weblog[link].backlinks.append(page)
+                weblog[link].backlinks.append(p)
 
     return weblog
 
-def filter_local_links(links):
-    return [link for link in links if not link.startswith('http')]
 
 def parse_args(args):
     parser = argparse.ArgumentParser(description='Simple weblog generator (with backlinks support!)')
@@ -58,8 +67,12 @@ def main(args=None):
     weblog = parse_weblog(args.website_path)
     # TODO further processing
 
+    for page_name, page in weblog.items():
+        print(f'{page_name} links={page.links} backlinks={page.backlinks}')
+
     # Useful for testing
     return weblog
+
 
 if __name__ == '__main__':
     main()
