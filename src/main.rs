53fdc2e995ca1d3215392e1c3240aa7b0416f1fd
Eduard Nicodei <eddnicodei@gmail.com>
2023-04-27 23:10:04 +0100

    Move parser code into separate file.
    Also rename post to page (and weblog to pages)
    Also use a Vec<Page> instead of a HashMap

--- src/main.rs
+++ src/main.rs
@@ -1,109 +1,15 @@
-use clap::{Parser};
-use regex::Regex;
+use clap::Parser;
+use parser::Page;
 use rouille::Response;
 use std::fs;
-use std::io::{BufRead, BufReader};
 use std::path::{PathBuf, Path};
 use std::str::FromStr;
 
 use minijinja::{Environment, Source, context, AutoEscape};
-use serde::Serialize;
 
-#[derive(Debug, Serialize)]
-struct Post {
-    name: String,
-    date_y_m_d: Option<(u32, u32, u32)>,
-    lines: Vec<String>,
-    body: String,
-    title: String,
-    links: Vec<String>,
-    backlinks: Vec<(String, String)>,
-}
+use crate::parser::load_pages;
 
-impl Post {
-    fn new(path: &PathBuf) -> Self {
-        let title_re = Regex::new(r"<h1>(.+)</h1>").unwrap();
-        // Use ? character to specify non-greedy matching (minimal)
-        let link_re = Regex::new(r#"<a href="(.+?)">"#).unwrap();
-        let post_name_re = Regex::new(r"((\d{4})-(\d{2})-(\d{2})-)?.+").unwrap();
-
-        let name = path.file_name().unwrap().to_str().unwrap().to_owned();
-        println!("name: {name:?}");
-        let parsed_name = post_name_re.captures(&name).unwrap();
-        let date_y_m_d = if let Some(_) = parsed_name.get(1) {
-            let year: u32 = parsed_name.get(2).unwrap().as_str().parse().unwrap();
-            let month: u32 = parsed_name.get(3).unwrap().as_str().parse().unwrap();
-            let day: u32 = parsed_name.get(4).unwrap().as_str().parse().unwrap();
-            Some((year, month, day))
-        } else {
-            None
-        };
-
-        let file = fs::File::open(path).unwrap();
-        let reader = BufReader::new(file);
-        let lines = reader.lines().map(|line| line.unwrap()).collect::<Vec<_>>();
-        let body = lines.join("");
-        println!("line[0] {:?} for file {:?}", lines[0], path.to_str());
-        let title = title_re.captures(&lines[0]).unwrap()[1].to_string();
-        let links = link_re
-            .captures_iter(&body)
-            .map(|cap| cap[1].to_string())
-            .collect::<Vec<_>>();
-
-        Post {
-            name,
-            date_y_m_d,
-            lines,
-            body,
-            title,
-            links,
-            backlinks: Vec::new(),
-        }
-    }
-}
-
-fn parse_site(top_path: &std::path::Path) -> std::collections::HashMap<String, Post> {
-    let post_paths = top_path
-        .read_dir()
-        .expect("Expected a directory")
-        .map(|entry| entry.unwrap().path())
-        .filter(|path| path.is_file() && path.extension().expect("Unable to get extension") == "html")
-        .collect::<Vec<_>>();
-    
-    let mut weblog = post_paths
-        .into_iter()
-        .map(|path| {
-            let post = Post::new(&path);
-            (post.name.clone(), post)
-        })
-        .collect::<std::collections::HashMap<_, _>>();
-    
-    let _weblog_clone = weblog.iter();
-    
-    // Create backlinks.
-    let backlink_pairs: Vec<(String, String, Vec<String>)> = weblog
-        .iter()
-        .map(|(name, post)| 
-            (name.clone(),
-             post.title.clone(),
-             post
-                .links 
-                .clone()
-                .iter()
-                .filter(|link| !link.starts_with("http"))
-                .map(|x| (*x).clone() )
-                .collect()))
-        .collect();
-         
-    for (name, title, links) in backlink_pairs {
-        println!("Name {name}");
-        for link in links {
-            println!("  Link: \"{}\"", &link);
-            weblog.get_mut(&link).unwrap().backlinks.push((name.clone(), title.clone()));
-        }
-    }
-    weblog
-}
+mod parser;
 
 #[derive(Parser,Debug)]
 #[command(author, version, about, long_about = None)]
@@ -115,25 +21,25 @@ struct Args {
     output_dir: String,
 }
 
-fn render_posts(
-    weblog: &std::collections::HashMap<String, Post>,
+fn render_pages(
+    pages: &Vec<Page>,
     env: &Environment,
     out_path: &PathBuf,
 ) {
     fs::create_dir_all(&out_path).unwrap();
 
-    for post in weblog.values() {
-        let template = env.get_template("post.html").unwrap();
-        let file = fs::File::create(out_path.join(&post.name)).unwrap();
-        template.render_to_write(context!{post => post}, file).unwrap();
+    for page in pages {
+        let template = env.get_template("page.html").unwrap();
+        let file = fs::File::create(out_path.join(&page.name)).unwrap();
+        template.render_to_write(context!{page}, file).unwrap();
     }
 }
 
 fn main() {
     let args = Args::parse();
     let website_path= Path::new(&args.website_path);
-    let weblog = parse_site(&website_path);
-    println!("{:?}", weblog.values()) ;
+    let pages = load_pages(&website_path);
+    println!("{:?}", pages) ;
     let templates_path = Path::new(&args.website_path.clone()).join("templates");
     
     let mut env = Environment::new();
@@ -143,18 +49,18 @@ fn main() {
     let template = env.get_template("page-list.html").unwrap();
     
     let output_dir = PathBuf::from_str(&args.output_dir).unwrap();
-    render_posts(&weblog, &env, &output_dir);
+    render_pages(&pages, &env, &output_dir);
     
     let file = fs::File::create(output_dir.join("page-list.html")).unwrap();
     template.render_to_write(
         context!
-            {posts => weblog.iter().collect::<Vec<_>>()},
+            {pages => pages},
         file).unwrap();
         
-    for post in weblog.values() {
+    for page in pages {
         println!(
             "{} links={:?} backlinks={:?} title=\"{}\"",
-            post.name, post.links, post.backlinks, post.title
+            page.name, page.links, page.backlinks, page.title
         );
     }
         
---
+++ src/parser.rs
@@ -0,0 +1,163 @@
+use regex::Regex;
+use serde::Serialize;
+
+type Title = String;
+type Name = String;
+
+#[derive(Debug, Serialize)]
+pub struct Page {
+    pub name: Name,
+    pub date_formatted: Option<String>,
+    pub body: String,
+    pub title: Title,
+    pub links: Vec<String>,
+    pub backlinks: Vec<(Name, Title)>,
+}
+
+impl Page {
+    fn new(path: &std::path::Path) -> Page {
+        let title_re = Regex::new(r"<h1>(.+?)</h1>").unwrap();
+        // Use ? character to specify non-greedy matching (minimal)
+        let link_re = Regex::new(r#"<a href="(.+?)">"#).unwrap();
+        let date_re = Regex::new(r"(\d{4})-(\d{2})-(\d{2})-.+").unwrap();
+        
+        let name = path.file_name().unwrap().to_str().to_owned()
+            .expect(&format!("Unable to extract file name for {:?}", path)).to_owned();
+        
+        let body = std::fs::read_to_string(path)
+            .expect(&format!("Unable to read {}", path.to_str().unwrap()));
+            
+        let title = title_re.captures(&body)
+            .expect(&format!("Unable to find title in {}", &body))[1].to_owned();
+        
+        let links : Vec<String> = link_re
+            .captures_iter(&body)
+            .map(|captures| captures[1].to_string())
+            // Get only unique links.
+            .collect::<std::collections::HashSet<_>>()
+            .into_iter()
+            .collect();
+        
+        let date = if let Some(captures) = date_re.captures(&name) {
+            let get_number = |index, element| {
+                captures
+                    .get(index)
+                    .expect(&format!("Unable to parse {element}"))
+                    .as_str()
+                    .parse()
+                    .expect("Unable to parse number")
+            };
+            Some(chrono::NaiveDate::from_ymd_opt(
+                get_number(1, "year"),
+                get_number(2, "month").try_into().unwrap(),
+                get_number(3, "day").try_into().unwrap()).unwrap())
+        } else {
+            None
+        };
+        
+        println!("Title: {}", title);
+        
+        Page {
+            name,
+            date_formatted: date.map(|d| d.format("%B %d, %Y").to_string()),
+            body,
+            title,
+            links,
+            backlinks: Vec::new(),
+        }
+    }
+}
+
+pub fn load_pages(top_path: &std::path::Path) -> Vec<Page> {
+    let mut pages_map= top_path
+        .read_dir().expect("Expected a directory")
+        .map(|entry| entry.unwrap().path())
+        .filter(|path| path.is_file() && path.extension().map_or(false, |extension| extension == "html"))
+        .map(|path| {
+            let page = Page::new(&path);
+            (page.name.clone(), page)
+        })
+        .collect::<std::collections::HashMap<_, _>>();
+    
+    // Create backlinks.
+    let backlinks : Vec<(Name, Title, Vec<Name>)> =
+        pages_map
+        .iter()
+        .map(|(name, page)|
+            (name.clone(),
+             page.title.clone(),
+             page.links.iter()
+                .filter_map(|link|
+                    // Only keep the links that don't start with http.
+                    if !link.starts_with("http") {
+                        Some((*link).clone())
+                    } else {
+                        None
+                    })
+                .collect()
+            ))
+        .collect();  // What are for loops?
+    
+    for (name, title, links) in backlinks {
+        println!("Backlinks for {name}");
+        for link in links {
+            println!("  Link: \"{link}\"");
+            pages_map.get_mut(&link).unwrap().backlinks.push((name.clone(), title.clone()));
+        }
+    }
+    
+    // Sort pages by name.
+    let mut pages : Vec<Page> = pages_map.into_values().collect();
+    pages.sort_by(|p1, p2| p1.name.cmp(&p2.name));
+    pages
+}
+
+#[cfg(test)]
+mod tests {
+    use super::load_pages;
+    
+    fn create_test_pages(dir: &std::path::Path) {
+        std::fs::write(
+            &dir.join("test_page_1.html"),
+            b"<h1>First Test page</h1>
+                <p>Some text with a <a href=\"test_page_2.html\">link</a></p>
+                <h1>Another title just to be confusing</h1>
+                <p>Some more text with another <a href=\"https://www.google.com\">link</a></p>
+                <a href=\"2022-02-22-test_page_3.html\">Another link</a>")
+            .expect("Unable to write page 1");
+            
+        std::fs::write(
+            &dir.join("test_page_2.html"),
+            b"<h1>Second Test Page</h1>
+                <p>Let's create a cycle <a href=\"test_page_1.html\">link</a></p>
+                <p>Let's create a cycle again <a href=\"test_page_1.html\">another link</a></p>")
+            .expect("Unable to write page 2");
+            
+        std::fs::write(
+            &dir.join("2022-02-22-test_page_3.html"),
+            b"<h1>Need a title element else it breaks</h1><br>
+                <p>Let's create a self cycle <a href=\"2022-02-22-test_page_3.html\">link</a></p>
+                <p>Let's create a another self cycle <a href=\"2022-02-22-test_page_3.html\">another link</a></p>")
+            .expect("Unable to write page 3");
+            
+        std::fs::write(
+            &dir.join("2022-02-23-test_page_4.html"),
+            b"<h1>Title</h1><br>
+                <p>Can't have enough <a href=\"2022-02-22-test_page_3.html\">links</a></p>")
+            .expect("Unable to write page 3");
+        
+    }
+
+    #[test]
+    fn new_page() {
+        let tmpdir = tempfile::tempdir().expect("Unable to create tempdir");
+        create_test_pages(&tmpdir.path());
+        let pages = load_pages(&tmpdir.path());
+        
+        println!("Pages should be sorted alphabetically");
+        assert_eq!(pages[0].name, "2022-02-22-test_page_3.html");
+        assert_eq!(pages[1].name, "2022-02-23-test_page_4.html");
+        assert_eq!(pages[2].name, "test_page_1.html");
+        assert_eq!(pages[3].name, "test_page_2.html");
+    }
+}
