df2768bdb2ca8f26b749dde17efa58937f5f5aff
eduard <eddnicodei@gmail.com>
2024-03-16 22:09:36 +0000

    First draft of flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "make digital garden";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            rust
+            rustfmt
+            cargo
+          ];
+        };
+      }
+    );
+}
